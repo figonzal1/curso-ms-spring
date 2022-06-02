c7023f036f99f9e148c4a26b2bcab2fa70f6a6e9
Felipe González Alarcón <felipe.gonzalezalarcon94@gmail.com>
2022-06-02 01:11:47 -0400

    correccion puerto eureka
    Signed-off-by: Felipe González Alarcón <felipe.gonzalezalarcon94@gmail.com>
    
    Took 10 minutes

--- eureka-ms/build.gradle.kts
+++ eureka-ms/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-starter")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap:3.1.2")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
